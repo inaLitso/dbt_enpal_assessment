36673ad98726af5c8d4ab1003e0b9d2ebc532a72
ina <inalitso@gmail.com>
2025-09-15 11:40:19 +0200

    add comments for call funnel

--- models/crm/intermediate/int_sales_call_funnel.sql
+++ models/crm/intermediate/int_sales_call_funnel.sql
@@ -10,9 +10,9 @@ SELECT
 FROM {{ ref('int_deal_stages_history') }} AS deals
 JOIN {{ ref('int_activity_history') }} AS act
     ON act.deal_id=deals.deal_id
-           AND deals.stage_started_at<due_to_at
+    --       AND deals.stage_started_at<due_to_at -- Needs confirmation. I believe that it is needed but just for the shake of having 2 activities
 JOIN {{ ref('stg_pipedrive_activity_types') }} AS act_types
     ON act_types.activity_type_group = act.activity_type
 WHERE deals.stage IN ('2','3')
     AND activity_type_group IN ('meeting', 'sc_2')
-    AND is_done = TRUE
+    AND is_done = TRUE -- Needs confirmation if it is needed but I assume that only activities that actually happened are needed
